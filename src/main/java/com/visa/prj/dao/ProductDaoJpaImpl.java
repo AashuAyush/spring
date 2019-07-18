3558de55080cc41295f42708fb95e09d83a1e7a1
aashu <aashutosh.ayush@gmail.com>
2019-07-18 11:54:15 +0000

    spring orderapp

---
+++ src/main/java/com/visa/prj/dao/ProductDaoJpaImpl.java
@@ -0,0 +1,39 @@
+package com.visa.prj.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+
+import com.visa.prj.entity.Product;
+
+@Repository
+public class ProductDaoJpaImpl implements ProductDao {
+	
+	@PersistenceContext //environment to manage entities @Entity
+	private EntityManager em ;
+	
+
+	@Override
+	public void addProduct(Product p) {
+//		System.out.println("Product added!!!");
+		em.persist(p);
+
+	}
+
+	@Override
+	public List<Product> getProducts() {
+		String jpql = "from Product";
+		TypedQuery<Product> query = em.createQuery(jpql, Product.class);
+		return query.getResultList();
+	}
+
+	@Override
+	public Product getProduct(int id) {
+		return em.find(Product.class, id);
+	}
+
+}
